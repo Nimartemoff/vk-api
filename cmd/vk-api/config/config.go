641c662ed981c9685b4cf88a1a2b37510485e256
Artem Nizamov <nizamov.artem2@wb.ru>
2024-12-14 11:13:57 +0500

    fix(vk-api): cfg

--- cmd/vk-api/config/config.go
+++ cmd/vk-api/config/config.go
@@ -7,15 +7,20 @@ import (
 
 type vkAPI struct {
 	URLs  []string `env-default:"https://api.vk.com,https://api.vk.ru"`
-	Token string   `env:"TOKEN" env-required:"true"`
+	Token string   `env:"TOKEN" env-default:"<TOKEN HERE>"`
+}
+
+type API struct {
+	Port string `env:"PORT" env-default:":8080"`
 }
 
 type neo4j struct {
-	URL    string `env:"URL" env-required:"true"`
+	URL    string `env:"URL" env-default:"bolt://localhost:7687"`
 	DBName string `env:"DB_NAME" env-default:"nizamov_vk"`
 }
 
 type Config struct {
+	API            API
 	VKAPI          vkAPI
 	Neo4j          neo4j
 	ContextTimeout time.Duration `env:"TIMEOUT" env-default:"60s"`
